36ba416d5a7770220ae249984c01d8073eb258c8
Djellza Asllani <diellza.asllani@student.cacttus.education>
2025-10-16 19:32:59 +0200

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,12 @@
+function loggedIn(){
+    alert('You now are logged in');
+}
+function sendCode(){
+    alert('We have sent you an email with the code!');
+   }
+   function welcome(){
+    alert('Welcome to our family!')
+}
+function Submit() {
+    alert('Thank you!')
+}
